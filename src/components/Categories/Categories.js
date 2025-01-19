6c10ed58e13ab79b9c6b51a94c46bd3d0b998875
Radek Kowalczyk <radekkowalczyk245@gmail.com>
2025-01-19 17:48:56 +0100

    zmiana categories

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,4 +1,5 @@
-import { React } from "react";
+import React from "react";
+import "./Categories.css";
 
 const Categories = () => {
   const categories = ["Action", "Comedy", "Drama", "Fantasy", "Sci-Fi"];
@@ -8,9 +9,15 @@ const Categories = () => {
       <h2>Categories</h2>
       <div className="categories-grid">
         {categories.map((category) => (
-          <div key={category} className="category-card">
-            <a href={`/categories/${category.toLowerCase()}`}>{category}</a>
-          </div>
+          <button
+            key={category}
+            className="category-button"
+            onClick={() =>
+              (window.location.href = `/categories/${category.toLowerCase()}`)
+            }
+          >
+            {category}
+          </button>
         ))}
       </div>
     </div>
